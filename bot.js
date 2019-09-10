4ef96820bbb7eed548930268aed060ad8dc84bbc
beboahmed <52512621+beboahmed@users.noreply.github.com>
2019-09-10 13:32:11 -0700

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("620832912429809697")
+setInterval(function() {
+channel.send(`king`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
